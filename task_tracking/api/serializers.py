170c34c76e90db1427b87053743210f347388338
KidIsKing <anulamasalova@gmail.com>
2026-05-18 16:03:00 +0500

    return main version

--- task_tracking/api/serializers.py
+++ task_tracking/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from tasks.models import Project, Task, Comment, Notification
+from tasks.models import Project, Task, Comment
 
 User = get_user_model()
 
@@ -91,12 +91,3 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ("id", "text", "created_at", "author", "task_id", "task")
         read_only_fields = ("id", "created_at", "author")
-
-
-class NotificationSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-
-    class Meta:
-        model = Notification
-        fields = ("id", "text", "user")
-        read_only_fields = ("id", "user")
--- task_tracking/api/urls.py
+++ task_tracking/api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, TaskViewSet, CommentViewSet, NotificationViewSet
+from .views import ProjectViewSet, TaskViewSet, CommentViewSet
 
 from django.urls import path, include
 
@@ -8,7 +8,6 @@ router = DefaultRouter()
 router.register(r"projects", ProjectViewSet, basename="project")
 router.register(r"tasks", TaskViewSet, basename="task")
 router.register(r"comments", CommentViewSet, basename="comment")
-router.register(r"notifications", NotificationViewSet, basename="notification")
 
 urlpatterns = [
     path('', include(router.urls)),
